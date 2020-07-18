09f3b524737c3d314d68571d85c8ae62d6f209fd
wangwc <wwc953@163.com>
2020-07-18 18:00:46 +0800

    测试新git

---
+++ src/main/java/com/activitymq/test/Test.java
@@ -0,0 +1,9 @@
+package com.activitymq.test;
+
+import org.apache.camel.component.xslt.XsltOutput;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hhh");
+    }
+}
